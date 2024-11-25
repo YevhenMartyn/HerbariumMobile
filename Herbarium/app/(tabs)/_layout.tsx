c3293726be6e1fc98bcce424824c5631c430995e
Yevhen Martyn <martyn.yevhen@gmail.com>
2024-11-25 23:38:30 +0100

    camera intermediate

---
+++ Herbarium/app/(tabs)/_layout.tsx
@@ -0,0 +1,33 @@
+import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+
+export default function TabsLayout() {
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          headerTitle: "Index",
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              name={focused ? "home-sharp" : "home-outline"}
+              size={30}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="cameraScreen"
+        options={{
+          headerTitle: "CameraScreen",
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              name={focused ? "camera-sharp" : "camera-outline"}
+              size={30}
+            />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
